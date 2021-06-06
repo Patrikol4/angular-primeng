ead1b8784658d605dc418617d1fe6458b3261c5e
Patrikol4 <doublecrt@gmail.com>
2021-06-06 10:32:27 -0300

    Tentativa de gravar dados em arquivo json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from './userService';
 import { User } from './user';
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,21 @@ export class AppComponent {
   title = 'angular-primeng';
 
   users: User[];
+  mostrarUserCriadoRecente: any;
+  dadosUser = {
+    id: Math.random().toString(36).substr(2,9),
+    nome: "",
+    senha: ""
+  }
   
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private http: HttpClient) { }
+
+  criarUser() {
+    this.mostrarUserCriadoRecente = this.http.post("assets/users-new.json", this.dadosUser)
+      .subscribe((value: any) => {
+        this.mostrarUserCriadoRecente = value;
+      })
+  }
 
   ngOnInit() {
     this.userService.getUserSmall().then(data => this.users = data);

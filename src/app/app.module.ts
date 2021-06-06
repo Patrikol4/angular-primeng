467bd9392efcbf94241b021d7986236136d033c1
Patrikol4 <doublecrt@gmail.com>
2021-06-06 01:32:34 -0300

    Finalizando tabela para exibir dados de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
-import {InputTextModule} from 'primeng/inputtext';
-import {DropdownModule} from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+
+import { UserService } from './userService';
+
 
 
 @NgModule({
@@ -17,9 +25,12 @@ import {DropdownModule} from 'primeng/dropdown';
     AppRoutingModule,
     ButtonModule,
     InputTextModule,
-    DropdownModule
+    DropdownModule,
+    TableModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

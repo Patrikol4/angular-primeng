cbbe3d00e33a80f7db5b53a99ad6f381ed2fcb47
Patrikol4 <doublecrt@gmail.com>
2021-06-06 00:17:06 -0300

    Mudando a versao do primeng e corrigindo erro com modulos da lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
